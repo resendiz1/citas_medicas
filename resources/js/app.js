c828ae6bcbdd4a06b73b71e5fc2240365d355c0b
devapppabsa-byte <devapppabsa@gmail.com>
2026-06-13 22:53:27 -0600

    Configure Laravel Echo with Reverb_2_

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,13 +7,17 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+console.log('REVERB KEY:', import.meta.env.VITE_REVERB_APP_KEY);
+console.log('REVERB HOST:', import.meta.env.VITE_REVERB_HOST);
+console.log('REVERB PORT:', import.meta.env.VITE_REVERB_PORT);
+console.log('REVERB SCHEME:', import.meta.env.VITE_REVERB_SCHEME);
 
     window.Echo = new Echo({
         broadcaster: 'reverb',
         key: import.meta.env.VITE_REVERB_APP_KEY,
         wsHost: import.meta.env.VITE_REVERB_HOST,
-        wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-        wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+        wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+        wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
         forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
         enabledTransports: ['ws', 'wss'],
     });
